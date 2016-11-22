c3e19823acb061c3d80a05322b68ef9580876c7a
boyrest <425728299@qq.com>
2016-11-22 17:12:54 +0800

    commit all files

---
+++ config/webpack.common.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    //我们在大多数现代浏览器中运行 Angular 程序时需要的标准填充物。
+    'polyfills': './src/polyfills.ts',
+    //我们需要的提供商文件： Angular 、 Lodash 、 bootstrap.css ……
+    'vendor': './src/vendor.ts',
+    //我们的应用代码
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+
+  module: {
+    loaders: [{
+      test: /\.ts$/,
+      loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+    }, {
+      test: /\.html$/,
+      loader: 'html'
+    }, {
+      test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+      loader: 'file?name=assets/[name].[hash].[ext]'
+    }, {
+      test: /\.css$/,
+      exclude: helpers.root('src', 'app'),
+      loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+    }, {
+      test: /\.css$/,
+      include: helpers.root('src', 'app'),
+      loader: 'raw'
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
