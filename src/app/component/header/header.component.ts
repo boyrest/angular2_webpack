f91000cfa339e17bfca572bc0472b48e3d5a8efb
Baron Zhu <baron.zhu@pwc.com>
2016-11-28 21:08:33 +0800

    add routes

---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import '../../../../public/css/styles.scss';
+@Component({
+  selector: "app-header",
+  templateUrl:"./header.component.html",
+  styleUrls:['./header.component.scss']
+})
+
+export class AppHeader{
+
+}
