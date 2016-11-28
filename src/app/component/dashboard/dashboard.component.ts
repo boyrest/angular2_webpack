253985b0ece29ae44bf1e67ab223284c54fda2f9
Baron Zhu <baron.zhu@pwc.com>
2016-11-28 15:24:05 +0800

    add dashboard

---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import '../../../../public/css/styles.scss';
+@Component({
+  selector: 'my-app',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+
+export class DashboardComponent { }
